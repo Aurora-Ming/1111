edeb1daf10291c8322bed755d208b3d658922e83
Aurora <3242576008@qq.com>
2023-02-15 17:31:48 +1300

    1

---
+++ main.py
@@ -0,0 +1,8 @@
+f=open("./DATA.csv","r",encoding="utf-8")
+(username,firstname,lastname,email)=f.readline().split(",",3)
+n=email.count("@")
+m=email.count(".")
+while (n！=1 or m<1 or m>2)
+   while ("./error-email.csv","w",encoding="utf-8")
+
+
