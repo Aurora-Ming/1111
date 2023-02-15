2b5c7adc55d5bdf26db9633b512671324c359e39
Aurora <3242576008@qq.com>
2023-02-15 17:53:29 +1300

    1

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-f=open("./DATA.csv","r",encoding="utf-8")
-(username,firstname,lastname,email)=f.readline().split(",",3)
-n=email.count("@")
+with open("./DATA.csv", "r", encoding="utf-8") as f:
+ (username, firstname, lastname, email) = f.readline().split(",", 3)
+n= email.count("@")
 m=email.count(".")
-while (n！=1 or m<1 or m>2)
-   while ("./error-email.csv","w",encoding="utf-8")
+int(n)
+int(m)
+while(n!=1) :
+    if (m<1 or m>2):
+      with open("./error-email.csv", "w", encoding="utf-8") as g:
+       g.write(email)
+    with open("./DATA.csv", "r", encoding="utf-8") as f:
+     (username, firstname, lastname, email) = f.readline().split(",", 3)
+
+with open("./email.csv", "w", encoding="utf-8") as h:
+    h.write(email)
+
+
+
+
+
+
+
 
 
