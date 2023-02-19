fb8fc794d7a88a5c62c0f4080039368a2993b19a
64210 <3242576008@qq.com>
2023-02-19 21:31:56 +1300

    111

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-with open("./DATA.csv", "r", encoding="utf-8") as f:
- (username, firstname, lastname, email) = f.readline().split(",", 3)
-n= email.count("@")
-m=email.count(".")
-int(n)
-int(m)
-while(n!=1) :
-    if (m<1 or m>2):
-      with open("./error-email.csv", "w", encoding="utf-8") as g:
-       g.write(email)
-    with open("./DATA.csv", "r", encoding="utf-8") as f:
-     (username, firstname, lastname, email) = f.readline().split(",", 3)
+f=open("./DATA.csv","r",encoding="utf-8")
+for name in f:
+ (name ,firstname,lastname,emails)=f.readline().split(",",3)
+ n=emails.count("@")
+ m=emails.count(".")
+ int(n)
+ int(m)
+ while(n!=1):
+  g=open("./invalid-email.csv","w",encoding="utf-8")
+  (username,email)=emails.split("@",1)
+  g.write(username,",",email)
+ while(m<1 or m>2):
+   g=open("./invalid-username.csv","w",encoding="utf-8")
+   (username,email)=emails.split("@",1)
+   g.write(username,",",email)
+else:
+  k=open("./valid-email.csv","w",encoding="utf-8")
+  (username,email)=emails.split("@",1)
+  k.write(username)
+  k.write(email)
 
-with open("./email.csv", "w", encoding="utf-8") as h:
-    h.write(email)
+
 
 
 
