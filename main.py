f6747c557990a094940abd2b23e45879a3bccab8
Aurora <3242576008@qq.com>
2023-02-20 20:29:59 +1300

    Done

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-f=open("./DATA.csv","r",encoding="utf-8")
-for name in f:
- (name ,firstname,lastname,emails)=f.readline().split(",",3)
- n=emails.count("@")
- m=emails.count(".")
- int(n)
- int(m)
- while(n!=1):
-  g=open("./invalid-email.csv","w",encoding="utf-8")
-  (username,email)=emails.split("@",1)
-  g.write(username,",",email)
- while(m<1 or m>2):
-   g=open("./invalid-username.csv","w",encoding="utf-8")
-   (username,email)=emails.split("@",1)
-   g.write(username,",",email)
-else:
-  k=open("./valid-email.csv","w",encoding="utf-8")
-  (username,email)=emails.split("@",1)
-  k.write(username)
-  k.write(email)
-
+f=open("./Programming - MOCK_DATA - Programming - MOCK_DATA.csv","r",encoding="utf-8")
+with open("./valid-email.csv","w",encoding="utf-8")as g:
+  g.write("[username domain]\n")
+with open("./invalid-email.csv","w",encoding="utf-8")as k:
+  k.write("[username domain]\n")
+line=f.readline()
+for line in f:
+  (username,firstname,lastname,emails)=line.split(",",3)
+  int=a=emails.count("@")
+  int=b=emails.count(".")
+  (name,domains)=emails.split("@",1)
+  (domain,other)=domains.split(".",1)
+  if a!=1:
+    with open("./invalid-email.csv","a",encoding="utf-8")as k:
+      k.write(emails)
+  elif b<1 or b>2:
+     with open("./invalid-email.csv","a",encoding="utf-8")as k:
+      k.write(emails)
+  else:
+      with open("./valid-email.csv","a",encoding="utf-8")as g:
+        g.write(name+" "+domain+"\n")
+      print(str(name+" "+domain))
 
 
 
